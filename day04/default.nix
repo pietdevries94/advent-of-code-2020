661af2dfbe07c580a22c7532bbc33afd0302625d
Piet de Vries <pietdevries94@gmail.com>
2026-01-22 22:29:06 +0100

    Day 4 - Part 2

--- day04/default.nix
+++ day04/default.nix
@@ -4,9 +4,30 @@
   input = builtins.readFile ./input;
   passports = lib.strings.splitString "\n\n" input;
 
-  passPortIsValid = p: lib.strings.hasInfix "byr:" p && lib.strings.hasInfix "iyr:" p && lib.strings.hasInfix "eyr:" p && lib.strings.hasInfix "hgt:" p && lib.strings.hasInfix "hcl:" p && lib.strings.hasInfix "ecl:" p && lib.strings.hasInfix "pid:" p;
+  makePairs = doc: builtins.map (x: builtins.match ''([a-z]{3}):(.*)'' x) (builtins.filter (x: x != []) (builtins.split ''[[:space:]]'' doc));
 
-  validPassports = builtins.filter passPortIsValid passports;
+  passportPairs = builtins.map makePairs passports;
+
+  getKeys = pairs: builtins.map (x: builtins.elemAt x 0) pairs;
+  allRequiredKeys = pair: (builtins.length (lib.lists.intersectLists ["byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"] (getKeys pair))) == 7;
+
+  validPassportPairsPart1 = builtins.filter allRequiredKeys passportPairs;
+
+  makeSet = pairs: builtins.foldl' (acc: cur: acc // {${builtins.elemAt cur 0} = builtins.elemAt cur 1;}) {} pairs;
+
+  toBeCheckedSets = builtins.map makeSet validPassportPairsPart1;
+
+  byrCheck = s: let i = builtins.fromJSON s.byr; in i >= 1920 && i <= 2002;
+  iyrCheck = s: let i = builtins.fromJSON s.iyr; in i >= 2010 && i <= 2020;
+  eyrCheck = s: let i = builtins.fromJSON s.eyr; in i >= 2020 && i <= 2030;
+  hgtCheck = s: let i = builtins.fromJSON (builtins.substring 0 ((builtins.stringLength s.hgt) - 2) s.hgt); in ((lib.strings.hasSuffix "cm" s.hgt) && i >= 150 && i <= 193) || ((lib.strings.hasSuffix "in" s.hgt) && i >= 59 && i <= 76);
+  hclCheck = s: (builtins.match ''#[0-9a-f]{6}'' s.hcl) != null;
+  eclCheck = s: builtins.elem s.ecl ["amb" "blu" "brn" "gry" "grn" "hzl" "oth"];
+  pidCheck = s: (builtins.match ''[0-9]{9}'' s.pid) != null;
+
+  allPart2Checks = s: (byrCheck s) && (iyrCheck s) && (eyrCheck s) && (hgtCheck s) && (hclCheck s) && (eclCheck s) && (pidCheck s);
+  validPassportSetsPart2 = builtins.filter allPart2Checks toBeCheckedSets;
 in {
-  part1 = builtins.length validPassports;
+  part1 = builtins.length validPassportPairsPart1;
+  part2 = builtins.length validPassportSetsPart2;
 }
